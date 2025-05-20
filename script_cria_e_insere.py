6aedeadfecd41c4be542400af5db2d18dc498591
Juan Citta <143374068+JuanCitta@users.noreply.github.com>
2025-05-20 18:39:51 -0300

    Update script_cria_e_insere.py
    - Inserção da data na tabela do usuário

--- script_cria_e_insere.py
+++ script_cria_e_insere.py
@@ -1,40 +1,51 @@
 import random
 from faker import Faker
 import psycopg2
-from datetime import datetime
+import datetime
+from datetime import timedelta
+from operator import itemgetter
 
 fake = Faker('pt_BR')
 
 DIAS_31 = [1,3,5,7,8,10,12]
 ELO_RANGE = 200
-NUM_USUARIOS = 20
-NUM_DIAS = 137
+NUM_USUARIOS = 10
 NUM_CLUBES = 5
+MAX_AMIGOS = 15
 TIPOS_DE_PARTIDA = ["Bullet", "Rápida","Padrão","Xadrez960","Por correspondência"]
-CARGOS = ["Administrador", "Membro"]
+CARGOS = ["Administrador", "Membro", "Membro", "Membro", "Membro", "Membro", "Membro", "Membro", "Membro", "Membro"]
+DATA_INICIO = datetime.date(2025,1,1)
+DATA_FIM = datetime.datetime.now().date()
+duration = DATA_FIM - DATA_INICIO
+NUM_DIAS = duration.days
 # Funcoes auxiliares
-
-
 def usuario():
     usuarios = []
     for i in range(1,NUM_USUARIOS+1):
         elo = 1000
         nome_usuario = fake.user_name()
         id = i
-        usuarios.append((id,nome_usuario,elo))
+        data_criado = fake.date_between(DATA_INICIO,DATA_FIM)
+        usuarios.append([id,nome_usuario,elo,data_criado])
     
     return usuarios
     
 def historico(usuarios):
-    mes = 1
+    #mes = 1
     historico = []
     id_partida = 1
     elo = []
-    for u in usuarios:
-        elo.append([u[2],u[0]])
-    elo.sort()
+    elo_usuario = {}
+    #Itera pelos dias entre a criacao do banco e a data atual, para facilitar a continuidade dos elos
     for d in range (1,NUM_DIAS+1):
-        for p in range(random.randint(1,4) * NUM_USUARIOS): 
+        delta = timedelta(d)     
+        offset = DATA_INICIO + delta 
+        #apenas adiciona os usuarios com data de criacao posterior a partida
+        for u in usuarios:
+            if u[3]> offset:
+                elo.append([u[2],u[0]])
+        elo.sort()
+        for p in range(abs(round((random.normalvariate(3,4)))) * NUM_USUARIOS): 
             p1 = random.randint(1,len(elo) - 1)
             p2 = p1-1 if elo[p1-1][0] - elo[p1][0] < ELO_RANGE else p1+1
             id_p1 = elo[p1][1]
@@ -47,7 +58,7 @@ def historico(usuarios):
                 if ganhador == p1:
                     res_p1 = 1
                     res_p1_string = "Vitória"
-                    res_p2 = 0
+                    res_p2 = 0  
                     res_p2_string = "Derrota"
                 else:
                     res_p1 = 0
@@ -65,7 +76,7 @@ def historico(usuarios):
             movimentos = round(random.normalvariate(35,5))
             precisao_p1 = round(random.normalvariate(55,5))
             precisao_p2 = round(random.normalvariate(55,5))
-            data = f"{d}/{mes}/{2025}"
+            data = offset
             tipo = random.choice(TIPOS_DE_PARTIDA)
             
             historico.append((id_p1,id_p2,id_partida,novo_elo_p1,precisao_p1,movimentos,res_p1_string, data, tipo))
@@ -73,32 +84,59 @@ def historico(usuarios):
             elo[p1][0] = novo_elo_p1
             elo[p2][0] = novo_elo_p2
             
-        if d == 28 and mes == 2:
-            mes = 3
-        elif d == 31 and mes in DIAS_31:
-            mes+= 1
-        elif d == 30:
-            mes+= 1
+            elo_usuario.update({id_p1 : novo_elo_p1})
+            elo_usuario.update({id_p2 : novo_elo_p2})
+            id_partida += 1
+        #if d == 28 and mes == 2:
+        #    mes = 3
+        #elif d == 31 and mes in DIAS_31:
+        #    mes+= 1
+        #elif d == 30:
+        #    mes+= 1
+
+    for id,elo in elo_usuario.items():
+        for u in usuarios:
+            if id == u[0]:
+                u[2] = elo
     return historico
             
+
 
 def clubes():
     clubes = []
     for clube in range(1,NUM_CLUBES + 1):
         id_clube = clube
         nome_clube = fake.village()
-        data_criacao = fake.date_this_year(before_today= True)
+        data_criacao = fake.date_between(DATA_INICIO,DATA_FIM)
         clubes.append((id_clube,nome_clube,data_criacao))
         
 def clube_usuario(usuarios,clubes):
     clube_usuario = []
     for c in clubes:
-        fundador = random.choice(usuarios[0])
+        users = usuarios.copy()
+        id_clube = c[0]
+        fundador = random.choice(users)
+        users.pop(fundador)
+        while(c[2] > fundador):
+            fundador = random.choice(usuarios)
         clube_usuario.append((c[0],fundador,c[2],"Fundador"))
         for u in range(1,abs(random.normalvariate(NUM_USUARIOS/5,NUM_USUARIOS/15))):
-            
-            data = fake.date_between_dates(c[2])
+            data_join = fake.date_between_dates(c[2])
+            cargo = random.choice(CARGOS)
+            usuario = random.choice(users)
+            users.pop(usuario)
+            id_usuario = usuario[0]
+            clube_usuario.append((id_clube,id_usuario,data_join,cargo))
+    return clube_usuario
 
+def amizades(usuarios):
+    amizades = []
+    users = usuarios.copy()
+    for u in users:
+
+        for a in range(0,random.randint(0,MAX_AMIGOS)):
+            data1 = u[3]
+    return amizades
 def inserir_no_banco():
     try:
         conn = psycopg2.connect(
@@ -181,7 +219,13 @@ def inserir_no_banco():
             conn.close()
 
 a = usuario()
-for e in a:
-    print(e)
+print(a)
 p = historico(a)
-print(p)
+
+#for h in p:
+#    if h[0] == 1:
+#        print(h)
+#for us in a:
+#    if us[0] == 1:
+#        print(us)
+#
